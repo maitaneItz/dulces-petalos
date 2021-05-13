ea0501352b7a243e4ec160c1debce291dc48fa23
Iñaki Ladrera <iladrera@oesia.com>
2021-05-13 17:08:37 +0200

    chore: elimina componente innecesario

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/¡Hola mundo!/i);
-  expect(linkElement).toBeInTheDocument();
-});
